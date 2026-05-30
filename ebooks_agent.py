5d8e44ffbcd7ee3989a5ecf8ed9ea8c815cef111
Jakob Edelstein <jakobedelstein@gmail.com>
2026-05-29 21:56:04 -0600

    Add comments

--- ebooks_agent.py
+++ ebooks_agent.py
@@ -8,8 +8,11 @@ from unix_socket import unix_socket
 
 
 async def _input_loop(agent: EbooksAgent):
+    # Continuously listen for inputs from an interactive shell
     while True:
         try:
+            # Create a new thread for the blocking input function
+            # Grab the search term from the shell
             term = (await asyncio.to_thread(input, "search> ")).strip()
         except EOFError:
             break
@@ -18,6 +21,9 @@ async def _input_loop(agent: EbooksAgent):
         if term.lower() in ("exit", "quit"):
             break
         try:
+            # Once we get a search term
+            # Pass it to the agent
+            # and await a response
             path = await agent.search(term)
             print(path)
         except TimeoutError as e:
@@ -25,18 +31,35 @@ async def _input_loop(agent: EbooksAgent):
 
 
 async def _main():
+    # Python's build in logging library
+    # Minimum log level is INFO (DEBUG messages are suppress)
+    # Format defines a message template.
     logging.basicConfig(level=logging.INFO, format="%(levelname)s %(message)s")
+    # Instantiate our ebooks agent
     agent = EbooksAgent()
+    # Wait for the agent to start and connect to the server
+    # We don't want to handle any input until it's ready to receive
     await agent.start()
 
+    # Spawn a new task to handle the unix socket
+    # Pass it a reference to the agent so we can feed
+    # it input from the socket
     socket_task = asyncio.create_task(unix_socket(agent))
     try:
+        # Start our input loop
+        # The unix socket will run on a different task
+        # while this loop continually listens for input
+        # from the CLI
         await _input_loop(agent)
     finally:
+        # When we exit out, we close our socket and end the task
+        # and disconnect agent from IRC server
         socket_task.cancel()
         agent.stop()
 
-
+# __name__ captures the module (entrypoint?) name
+# Detects whether this module was run as such
+# rather than imported into another. 
 if __name__ == "__main__":
     try:
         asyncio.run(_main())
